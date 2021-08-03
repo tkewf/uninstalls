a3445850f148869606aa499fa55b4ef2498407ab
Tiffany Ke <88349879+tkewf@users.noreply.github.com>
2021-08-03 18:53:16 -0400

    Update test.py

--- test.py
+++ test.py
@@ -82,6 +82,6 @@ def tree_new(input_variables): #max depth = 8, features = 5
 if st.sidebar.button("Click Here to Classify"):
     prediction = tree_new(parameter_input_values)
     if prediction == 0 :
-        st.markdown("# :point_left: There is a 93% chance that this person has **_not_** uninstalled the WFUS App.")
+        st.markdown("### :point_left: There is a 93% chance that this person has **_not_** uninstalled the WFUS App.")
     else:
         st.markdown("# There is a 93% chance that this person has **_uninstalled_** the WFUS App.")
