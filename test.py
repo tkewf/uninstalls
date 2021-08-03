9c82fe16b4f162b3dfdcc72330dc7800f4d6bdeb
tkewf <88349879+tkewf@users.noreply.github.com>
2021-08-03 15:36:55 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 st.sidebar.title("Features")
 #Intializing
-parameter_list=['Sepal length (cm)','Sepal Width (cm)','Petal length (cm)','Petal Width (cm)']
+parameter_list=['Average Total Page Views (per session)','Batch Delivers Received last week','Days since Last App Visit','Days since First App Visit', 'Number of App Visits last week']
 parameter_input_values=[]
-parameter_default_values=['5.2','3.2','4.2','1.2']
+parameter_default_values=['0','0','0','0','0']
 values=[]
 
 #Display
 for parameter, parameter_df in zip(parameter_list, parameter_default_values):
  
- values= st.sidebar.slider(label=parameter, key=parameter,value=float(parameter_df), min_value=0.0, max_value=8.0, step=0.1)
+ values= st.sidebar.slider(label=parameter, key=parameter,value=float(parameter_df), min_value=0.0, max_value=500, step=1)
  parameter_input_values.append(values)
  
 input_variables=pd.DataFrame([parameter_input_values],columns=parameter_list,dtype=float)
