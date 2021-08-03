0af0bf06e2d517bd09a8c606a6594f625e74c369
Tiffany Ke <88349879+tkewf@users.noreply.github.com>
2021-08-03 18:04:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -81,13 +81,6 @@ if __name__ == "__main__":
  if st.button("Click Here to Classify", ):
     prediction = tree_new(parameter_input_values)
     if prediction == 0 :
-     st.title(annotated_text('There is a 93% chance that this person has ',('not','','#F0E68C'	),'uninstalled the WFUS App.'))
+     st.title('There is a 93% chance that this person has **_not_** uninstalled the WFUS App.')
     else:
      st.title("There is a 93% chance that this person has **_uninstalled_** the WFUS App.")
-
-  
-  
-  
-  
-  
-  
