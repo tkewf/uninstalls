4d4f394604d7ab8258881dd0d1a6d8873a188b35
Tiffany Ke <88349879+tkewf@users.noreply.github.com>
2021-08-03 17:41:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
+background = Image.open('wayfairapp.png')
 
 st.sidebar.title("Features")
-st.sidebar.image('wayfairapp.png', width=100, height=100)
+st.sidebar.image(background, width=100, height=100)
 #Intializing
 parameter_list_slider=['Days since Last App Visit','Days since First App Visit']
 parameter_list_num=['Average Total Page Views (per session)','Batch Delivers Received last week','Number of App Visits last week']
