823614201dc1eee058268ddefbd3813c6ec5901a
tkewf <88349879+tkewf@users.noreply.github.com>
2021-08-03 15:30:50 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+st.sidebar.title("Features")
+#Intializing
+parameter_list=['Sepal length (cm)','Sepal Width (cm)','Petal length (cm)','Petal Width (cm)']
+parameter_input_values=[]
+parameter_default_values=['5.2','3.2','4.2','1.2']
+values=[]
