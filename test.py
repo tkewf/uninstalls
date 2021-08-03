9d56c7bdea91c4a92f936819e9c20aa0ff15d2b5
tkewf <88349879+tkewf@users.noreply.github.com>
2021-08-03 15:31:33 -0400

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,12 @@ parameter_list=['Sepal length (cm)','Sepal Width (cm)','Petal length (cm)','Peta
 parameter_input_values=[]
 parameter_default_values=['5.2','3.2','4.2','1.2']
 values=[]
+
+#Display
+for parameter, parameter_df in zip(parameter_list, parameter_default_values):
+ 
+ values= st.sidebar.slider(label=parameter, key=parameter,value=float(parameter_df), min_value=0.0, max_value=8.0, step=0.1)
+ parameter_input_values.append(values)
+ 
+input_variables=pd.DataFrame([parameter_input_values],columns=parameter_list,dtype=float)
+st.write('\n\n')
