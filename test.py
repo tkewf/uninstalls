e331274577c43ba87fbd05f2f76801949888081c
Tiffany Ke <88349879+tkewf@users.noreply.github.com>
2021-08-06 15:04:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -120,7 +120,7 @@ if st.sidebar.button("Click Here to Classify"):
     ax[0].set_ylim(top=10000)
     ax[0].set_xlabel('Days')
     ax[0].set_ylabel('Number of Users')
-    ax[0].axvline(parameter_input_variables[3], 0, 10000, label='User value')
+    ax[0].axvline(parameter_input_values[3], 0, 10000, label='User value')
 
     ax[1].hist(df_persisting_sample['days_since_firstappvisit'],range=[1,400],bins=40, color='skyblue')
     ax[1].set_title('Persisting users')
