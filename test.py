a9d5417c42fdfd1c0e28a542ae213796acc1169c
Tiffany Ke <88349879+tkewf@users.noreply.github.com>
2021-08-06 14:36:03 -0400

    Update test.py

--- test.py
+++ test.py
@@ -111,7 +111,7 @@ if st.sidebar.button("Click Here to Classify"):
     
     # First graph: Days since last visit
     st.markdown('### Days since first app visit')
-    fig, ax = plt.subplots(1,2, figsize=(3,6))
+    fig, ax = plt.subplots(1,2, figsize=(5,10))
 
     fig.suptitle('Days since Install', fontsize=20)
 
