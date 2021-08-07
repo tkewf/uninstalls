399f6704011b0250560a55ea0384e6c1d39fd79f
Tiffany Ke <88349879+tkewf@users.noreply.github.com>
2021-08-07 12:48:07 -0400

    Update test.py

--- test.py
+++ test.py
@@ -286,7 +286,7 @@ if st.sidebar.button("Click Here to Classify"):
         days_input = parameter_input_values[1]
         median_days_input = statistics.median(df_uninstalled_sample.days_since_firstappvisit)
         st.markdown("It has been **{} days** since this user installed the app,"
-                    "compared to the median number of days (**{}**) for **persisting** users.".format(days_input, median_days_input))
+                    "compared to the median number of days (**{}**) for **uninstalled** users.".format(days_input, median_days_input))
         st.write('')
         st.write('')  
         # Second graph: Days since last app visit
@@ -316,7 +316,7 @@ if st.sidebar.button("Click Here to Classify"):
         last_days_input = parameter_input_values[0]
         median_last_days_input = statistics.median(df_uninstalled_sample.days_since_lastappvisit)
         st.markdown("It has been **{} days** since this user last visited the app,"
-                    "compared to the median number of days (**{}**) for **persisting** users.".format(last_days_input, median_last_days_input))
+                    "compared to the median number of days (**{}**) for **uninstalled** users.".format(last_days_input, median_last_days_input))
         
         st.write('')
         st.write('')  
@@ -347,7 +347,7 @@ if st.sidebar.button("Click Here to Classify"):
         avg_page_views = parameter_input_values[2]
         median_avg_page_views = statistics.median(df_uninstalled_sample.AvgTotalPageViews)
         st.markdown("The average number of pages viewed during the past week for this user is **{}**, "
-                    "compared to the median number of pages (**{}**) for **persisting** users.".format(avg_page_views, median_avg_page_views))
+                    "compared to the median number of pages (**{}**) for **uninstalled** users.".format(avg_page_views, median_avg_page_views))
         
         st.write('')
         st.write('')  
@@ -378,5 +378,5 @@ if st.sidebar.button("Click Here to Classify"):
         num_visits = parameter_input_values[3]
         median_num_visits = statistics.median(df_uninstalled_sample.num_app_visits)
         st.markdown("The number of app visits during the past week for this user is **{}**, "
-                    "compared to the median number of visits (**{}**) for **persisting** users.".format(num_visits, median_num_visits))
+                    "compared to the median number of visits (**{}**) for **uninstalled** users.".format(num_visits, median_num_visits))
 
